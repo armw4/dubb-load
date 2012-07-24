d6812a205f57df1bde76e7094764b0ae91a679f2
Antwan "A-Dubb" Wimberly <awimberly484@gmail.com>
2012-07-23 20:41:43 -0400

    Upgrade website to MVC4.

--- FinancialPowerDemo/Properties/AssemblyInfo.cs
+++ FinancialPowerDemo/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8007fc84-24ed-43e4-93f9-45b1da5323fb")]
+[assembly: Guid("91e9c9cb-9ad5-4f10-91bf-3c2edd9192b2")]
 
 // Version information for an assembly consists of the following four values:
 //
