82d07800b8a458f62625c8d6e33e442e81640635
unknown <ADubb@ADubb-PC.(none)>
2012-07-25 01:11:20 -0400

    Add uncomitted changes from previous commit.

---
+++ FinancialPowerDemo.Configuration/Migrations/Up/1-Create-Database-DubbLoad.sql
@@ -0,0 +1,5 @@
+USE [master]
+GO
+
+/****** Object:  Database [DubbLoad]    Script Date: 07/24/2012 00:09:23 ******/
+CREATE DATABASE [DubbLoad]
---
+++ FinancialPowerDemo.Configuration/Migrations/Up/2-Create-Schema-FileAuthorization.sql
@@ -0,0 +1,8 @@
+USE [DubbLoad]
+GO
+
+/****** Object:  Schema [FileAuthorization]    Script Date: 07/24/2012 00:10:20 ******/
+CREATE SCHEMA [FileAuthorization] AUTHORIZATION [dbo]
+GO
+
+
